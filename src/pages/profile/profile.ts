9c614107467c2cb7f2355ff6537f8af5bdc553ca
Maphuti Setati <maphuti.setati@standardbank.co.za>
2020-02-01 15:10:25 +0200

    fix dashboard and profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,10 @@ import { STORAGE_KEY, COLLECTION } from '../../utils/consts';
 import { User } from '../../models/user';
 import { Ratings } from '../../models/ratings';
 import { bounceIn } from '../../utils/animations';
+import { Photo } from '../../models/photo';
+import { MediaProvider } from '../../providers/media/media';
+import { FeedbackProvider } from '../../providers/feedback/feedback';
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -13,13 +17,17 @@ import { bounceIn } from '../../utils/animations';
   animations: [bounceIn]
 })
 export class ProfilePage {
-  user : User;
+  profile : User;
   img: string;
   userRating = 0;
   allRatings: any = [];
 
   images = [];
-  constructor(public navCtrl: NavController, public dataProvider: DataProvider) {
+  constructor(
+    public navCtrl: NavController, 
+    public dataProvider: DataProvider,
+    public mediaProvider: MediaProvider,
+    public feedbackProvider: FeedbackProvider) {
   }
 
   ionViewDidLoad() {
@@ -36,45 +44,79 @@ export class ProfilePage {
       'assets/imgs/users/user9.jpg',
       'assets/imgs/users/user10.jpg',
     ]
-    this.user = this.dataProvider.getItemFromLocalStorage(STORAGE_KEY.user);
+    this.profile = this.dataProvider.getItemFromLocalStorage(STORAGE_KEY.user);
+    console.log(this.profile);
+    
     this.img = `assets/imgs/users/user1.jpg`;
     this.dataProvider.getAllFromCollection(COLLECTION.ratings).subscribe(ratingsFromCollection => {
       const r = this.dataProvider.getArrayFromObjectList(ratingsFromCollection);
       this.allRatings = this.dataProvider.getArrayFromObjectList(r);
-      // this.isLoading = false;
-      this.userRating = this.calculateUserRating();
       console.log(this.userRating);
     }); 
-  }
-
-  calculateUserRating() {
-    const index = this.getCompanyIndex(this.user);
-    if (index !== -999) {
-      delete this.allRatings[index].id;
-      const arrayRatingData = this.allRatings[index];
-      return parseFloat(this.dataProvider.calculateRating(
-        this.dataProvider.getArrayFromObjectList(arrayRatingData)).toFixed(1));
-    } else {
-      return 0;
-    }
-  }
-
-  getCompanyIndex(user: User): number {
-    let index = -999;
-    this.allRatings.forEach((r, i) => {
-      if (r[0] && user && r[0].id === user.uid) {
-        index = this.allRatings.indexOf(r);
-      }
-    });
-    return index;
-  }
-
+  } 
   getAge(date: string): string {
     return this.dataProvider.getAgeFromDate(date);
   }
 
   getDistance(geo) {
     return this.dataProvider.getLocationFromGeo(geo);
+  }
+
+  hasImages(): boolean {
+    return this.images && this.images.length > 0;
+  }
+
+  // downloadImages() {
+  //   this.oldImages.forEach(img => {
+  //     this.mediaProvider.getImage(img.url).then(resImg => {
+  //       this.images.push(resImg);
+  //       console.log(resImg);
+        
+  //     }).catch(err => {
+  //       console.log(err);
+  //     });
+  //   });
+  // }
+
+  addPhoto() {
+    const newImage: Photo = {dateCreated: this.dataProvider.getDateTime(), url: 'photo2.jpg'};
+    this.dataProvider.addItemToUserDB(COLLECTION.images, this.profile, newImage);
+  }
+
+  getImages() {
+    this.dataProvider.getAllFromCollection(COLLECTION.images).subscribe(imgs => {
+      console.log(imgs);
+    })
+  }
+ 
+  selectPhotoAndUpload() {
+    this.mediaProvider.selectPhoto().then(imageData => {
+      const captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+      // this.uploadPhotoAndUpdateUserDatabase(this.oldImages, captureDataUrl);
+    }, error => {
+      console.log("ERROR -> " + JSON.stringify(error));
+    });
+  }
+
+
+  uploadPhotoAndUpdateUserDatabase(oldImages, newImage): any {
+    this.feedbackProvider.presentLoading('Please wait, Uploading...');
+    let storageRef = firebase.storage().ref();
+    const filename = Math.floor(Date.now() / 1000);
+    const imageRef = storageRef.child(`${this.profile.uid}/${filename}.jpg`);
+
+    imageRef.putString(newImage, firebase.storage.StringFormat.DATA_URL).then(()=> {
+      this.feedbackProvider.dismissLoading();
+      const newImageObject: Photo = {
+        url: filename+'.jpg',
+        dateCreated: this.dataProvider.getDateTime()
+      }
+      this.dataProvider.addItemToUserDB(COLLECTION.images, this.profile, newImageObject);
+    }).catch(err => {
+      this.feedbackProvider.dismissLoading();
+      this.feedbackProvider.presentToast('Image upload failed');
+    });
+
   }
 
 
